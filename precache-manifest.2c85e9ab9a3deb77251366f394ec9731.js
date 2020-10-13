3b4af0dc60a852c732e2a15b1f1e779f3ad1ee89
thanh ngoc <ngocthanh19011998@gmail.com>
2020-10-13 15:18:57 +0700

    Updates

---
+++ precache-manifest.2c85e9ab9a3deb77251366f394ec9731.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "830c247c83abb2e2dad25746c6f2cd71",
+    "url": "/Employee_Manager/index.html"
+  },
+  {
+    "revision": "a122affc84cfd1a520ab",
+    "url": "/Employee_Manager/static/css/2.63992a86.chunk.css"
+  },
+  {
+    "revision": "220d56e9a164642269ef",
+    "url": "/Employee_Manager/static/css/main.afb178dd.chunk.css"
+  },
+  {
+    "revision": "a122affc84cfd1a520ab",
+    "url": "/Employee_Manager/static/js/2.08d742d1.chunk.js"
+  },
+  {
+    "revision": "7e8661572dbf62ca66d7837115f59618",
+    "url": "/Employee_Manager/static/js/2.08d742d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220d56e9a164642269ef",
+    "url": "/Employee_Manager/static/js/main.712cfd8b.chunk.js"
+  },
+  {
+    "revision": "b22c7df06394dd1d6f25",
+    "url": "/Employee_Manager/static/js/runtime-main.0f1b9c95.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/Employee_Manager/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/Employee_Manager/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/Employee_Manager/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/Employee_Manager/static/media/notification.c0d3c94c.eot"
+  }
+]);
